abc1fb4686c00131c78d3f90894acdf26d935a43
Andreas Traa Utkilen <andreastraautkilen@gmail.com>
2020-12-02 13:08:43 +0100

    minor fixes

--- src/main/resources/services/contact-form-service/contact-form-service.js
+++ src/main/resources/services/contact-form-service/contact-form-service.js
@@ -4,22 +4,12 @@ const libs = {
 };
 
 exports.post = (req) => {
-<<<<<<< HEAD
-    let form = JSON.parse(req.body).data;
-    let body = "";
-    for (const key in form) {
-        body += form[key] + "<br>"
-    }
-    log.info(form)
-    var status = libs.mailLib.send({
-=======
     const form = JSON.parse(req.body).data;
     const body = "";
     for (const key in form) {
         body += `${form[key]}<br>`;
     }
     const status = libs.mailLib.send({
->>>>>>> c8b02a60a71e37b0cf1059d110e8759d7c739485
         from: 'me@enonic.com',
         to: form.contact_email,
         subject: 'Test subject',
